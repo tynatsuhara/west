19bab0985f0ee0257b448b523c7c802dd879ccce
Tyler Bonnell <tbonnell@uw.edu>
2017-04-19 17:56:25 -0700

    start visual map

---
+++ Assets/Scripts/World/VisualMap.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisualMap : MonoBehaviour {
+
+	private static float scale = .1f;
+	public GameObject map;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+	
+	}
+}
