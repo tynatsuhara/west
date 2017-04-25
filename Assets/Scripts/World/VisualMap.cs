e60fb731db9b068ac0e96128f5811a0b74d58899
Tyler Bonnell <tbonnell@uw.edu>
2017-04-25 16:08:41 -0700

    comment out for now

--- Assets/Scripts/World/VisualMap.cs
+++ Assets/Scripts/World/VisualMap.cs
@@ -9,12 +9,12 @@ public class VisualMap : MonoBehaviour {
 	public Text txt;
 
 	void Start () {
-		Map m = SaveGame.currentGame.map;
-		foreach (var kv in m.locations) {
-			Vector3 pos = kv.Value.worldLocation.val;
-			GameObject go = Instantiate(txt, transform.position, txt.transform.rotation) as GameObject;
-			go.GetComponent<Text>().text = (kv.Value.icon.Length > 0 ? kv.Value.icon.Length + "\n" : "") + kv.Value.name;
-		}
+		// Map m = SaveGame.currentGame.map;
+		// foreach (var kv in m.locations) {
+		// 	Vector3 pos = kv.Value.worldLocation.val;
+		// 	GameObject go = Instantiate(txt, transform.position, txt.transform.rotation) as GameObject;
+		// 	go.GetComponent<Text>().text = (kv.Value.icon.Length > 0 ? kv.Value.icon.Length + "\n" : "") + kv.Value.name;
+		// }
 	}
 	
 	void Update () {
