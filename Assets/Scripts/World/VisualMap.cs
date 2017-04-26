55e919ccdc912efcb334fe7344623f896b5f56d5
Tyler Bonnell <tbonnell@uw.edu>
2017-04-25 23:25:22 -0700

    map edge spacing

--- Assets/Scripts/World/VisualMap.cs
+++ Assets/Scripts/World/VisualMap.cs
@@ -9,7 +9,7 @@ public class VisualMap : MonoBehaviour {
 	void Start () {
 		Map m = SaveGame.currentGame.map;
 		float scale = 1f/Map.WORLD_COORD_SIZE;
-		Vector3 offset = new Vector3(transform.localScale.x/2f, 0, transform.localScale.z/2f);	
+		Vector3 offset = new Vector3(transform.localScale.x, 0, transform.localScale.z) * txt.transform.parent.localScale.x / 2f;	
 		foreach (var kv in m.locations) {
 			GameObject newtxt = Instantiate(txt.gameObject, transform.position, txt.transform.rotation) as GameObject;
 			newtxt.transform.SetParent(txt.transform.parent);
