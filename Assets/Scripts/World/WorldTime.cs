13339429c5f0f671afab6af87956d1b11d829539
Tyler Bonnell <tbonnell@uw.edu>
2017-05-12 16:27:16 -0700

    im a good programmer

--- Assets/Scripts/World/WorldTime.cs
+++ Assets/Scripts/World/WorldTime.cs
@@ -1,9 +1,24 @@
+using UnityEngine;
+
 [System.Serializable]
 public class WorldTime {
-    public const float MINUTE = 1f;  // how many real life seconds represent an in-game minute?
+    public const float MINUTE = 1.5f;  // how many real life seconds represent an in-game minute?
     public const float HOUR = 60 * MINUTE;
     public const float DAY = 24 * HOUR;
     public const float YEAR = 365 * DAY;
 
-    public float worldTime;
+    private float worldTime_;    
+    public float worldTime {
+        get {
+            return worldTime_;
+        }
+        set {
+            int beforeTime = (int)(worldTime_/MINUTE);
+            worldTime_ = value;
+            int afterTime = (int)(worldTime_/MINUTE);
+            if (afterTime > beforeTime) {
+                Debug.Log(afterTime);
+            }
+        }
+    }
 }
