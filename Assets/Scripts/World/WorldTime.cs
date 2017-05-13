ab32d18527a9b3f8681714ef8f0c9c4156c0a463
Tyler Bonnell <tbonnell@uw.edu>
2017-05-12 17:07:37 -0700

    time

--- Assets/Scripts/World/WorldTime.cs
+++ Assets/Scripts/World/WorldTime.cs
@@ -7,6 +7,13 @@ public class WorldTime {
     public const float DAY = 24 * HOUR;
     public const float YEAR = 365 * DAY;
 
+    private int startYear = Random.Range(1875, 1885);
+    public WorldTime() {
+        worldTime_ = Random.Range(0, 365) * DAY +
+                     Random.Range(8, 4) * HOUR +  // between 8AM-4PM
+                     Random.Range(0, 60) * MINUTE;
+    }
+
     private float worldTime_;    
     public float worldTime {
         get {
@@ -17,7 +24,25 @@ public class WorldTime {
             worldTime_ = value;
             int afterTime = (int)(worldTime_/MINUTE);
             if (afterTime > beforeTime) {
-                Debug.Log(afterTime);
+                int year = (int)(worldTime/YEAR) + startYear;
+                int day = (int)(worldTime_ % YEAR/DAY);
+                int hour = (int)(worldTime_ % DAY/HOUR);
+                int minute = (int)(worldTime_ % HOUR/MINUTE);
+
+                var months = new string[]{"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+                var dayAmounts = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+                int daysSoFar = 0;
+                int month = 0;
+                for (;month < 12; month++) {
+                    if (day < daysSoFar + dayAmounts[month]) {
+                        day %= daysSoFar;
+                        month--;
+                        break;
+                    }
+                    daysSoFar += dayAmounts[month];
+                }
+                string date = string.Format("{0} {1}, {2} {3}:{4}", months[month], day, year, hour, (minute < 10 ? "0" : "") + minute);
+                Debug.Log(date);
             }
         }
     }
