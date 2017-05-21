50d3d032e6bcb3c32088fa4b698cf1dd9eeb6634
Tyler Bonnell <tbonnell@uw.edu>
2017-05-20 17:16:54 -0700

    hide load button if can't load

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -1,14 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class MainMenu : Menu {
 
-	public MenuNode newGame;
 	public MenuNode loadGame;
+	public MenuNode newGame;
 	public MenuNode quitGame;
 
-	void Start() {
+	void Awake() {
+		if (!SaveGame.SaveGameExists()) {
+			quitGame.down = newGame;
+			Destroy(loadGame.gameObject);
+			selectedNode = newGame;
+			newGame.transform.Translate(transform.up * 40f);
+			quitGame.transform.Translate(transform.up * 40f);
+		}
 	}
 
 	public override void Enter(MenuNode node) {
