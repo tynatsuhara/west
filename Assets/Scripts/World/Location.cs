d34221b94a5cf6a94541b485f5af108699b0b951
Tyler Bonnell <tbonnell@uw.edu>
2017-05-26 22:59:41 -0700

    remove old code

--- Assets/Scripts/World/Location.cs
+++ Assets/Scripts/World/Location.cs
@@ -37,10 +37,8 @@ public class Location {
 	public void Connect(Location l) {
 		int index = System.Array.IndexOf(connections, System.Guid.Empty);
 		connections[index] = l.guid;
-
-		// teleporters.Add(new Teleporter.TeleporterData(l.guid, new Vector3(index * 4, 1, -5)));
 	}
-	
+
 	public float DistanceFrom(Location l) {
 		return (l.worldLocation.val - worldLocation.val).magnitude;
 	}
