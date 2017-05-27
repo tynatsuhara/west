19c2cd6f8d6e68c665c8efd7cce8ce2c29253771
Tyler Bonnell <tbonnell@uw.edu>
2017-05-26 23:37:34 -0700

    more tumblin
    (have yet 2 test)

--- Assets/Scripts/World/Atmosphere/Tumbleweed.cs
+++ Assets/Scripts/World/Atmosphere/Tumbleweed.cs
@@ -1,13 +1,34 @@
 using UnityEngine;
+using System.Collections;
 
 public class Tumbleweed : MonoBehaviour {
-    public static Vector3 wind;
+    private Vector3 wind;
+    private Vector3 nextWind;
+    public float windForce;
 
-    public void Awake() {
-        if (wind == Vector3.zero) {
-            wind = Random.insideUnitSphere;
-            wind.y = 0f;
-            wind = wind.normalized;
+    public void Start() {
+       wind = NewDirection();
+       StartCoroutine(UpdateWind());
+    }
+
+    public void Update() {
+        wind = Vector3.Lerp(wind, nextWind, .2f);
+    }
+
+    public void FixedUpdate() {
+        GetComponent<Rigidbody>().AddForce(wind * windForce, ForceMode.Force);
+    }
+
+    private IEnumerator UpdateWind() {
+        while (true) {
+            yield return new WaitForSeconds(Random.Range(5, 10));
+            nextWind = NewDirection();
         }
+    }
+
+    private Vector3 NewDirection() {
+        Vector3 w = Random.insideUnitSphere;
+        w.y = 0f;
+        return w.normalized;
     }
 }
