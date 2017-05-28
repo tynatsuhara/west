ea5257dce032005028c54e0ae1efead4cabf9c40
Tyler Bonnell <tbonnell@uw.edu>
2017-05-28 12:03:56 -0700

    buildin

---
+++ Assets/Scripts/World/Building.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+// Represents a building exterior
+public class Building : MonoBehaviour {
+    public int width;
+    public int height;
+    public Vector2 doorTilePosition;  // where to build the road to
+}
