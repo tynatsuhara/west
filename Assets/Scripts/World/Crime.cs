488fdfe06970f1d4e006cfb88ab30251aa63de37
Tyler Bonnell <tbonnell@uw.edu>
2017-07-07 19:27:21 -0700

    add wanted status

--- Assets/Scripts/World/Crime.cs
+++ Assets/Scripts/World/Crime.cs
@@ -4,6 +4,13 @@ using System.Linq;
 
 [System.Serializable]
 public class Crime {
+    public enum WantedStatus {
+        NOT_WANTED,
+        ALIVE,  // not used right now, maybe later
+        DEAD_OR_ALIVE
+    }
+
+    // maps character guid -> location guid -> bounty
     private Dictionary<System.Guid, Dictionary<System.Guid, List<SingleCrime>>> committed = new Dictionary<System.Guid, Dictionary<System.Guid, List<SingleCrime>>>();
 
     public void Commit(System.Guid character, System.Guid location, string crime, int bounty) {
@@ -18,13 +25,32 @@ public class Crime {
         Debug.Log("Committed crime of " + crime.ToUpper() + ", bounty += " + bounty);
     }
 
+    public int Bounty(System.Guid character) {
+        if (!committed.ContainsKey(character)) {
+            return 0;
+        }
+        int result = 0;
+        foreach (System.Guid location in committed[character].Keys) {
+            result += committed[character][location].Select(x => x.bounty).Aggregate((x, y) => x + y);
+        }
+        return result;
+    }
+
     public int Bounty(System.Guid character, System.Guid location) {
         if (!committed.ContainsKey(character) || !committed[character].ContainsKey(location))
             return 0;
         return committed[character][location].Select(x => x.bounty).Aggregate((x, y) => x + y);
     }
 
-    [System.Serializable]    
+    public WantedStatus GetWantedStatus(System.Guid character) {
+        if (!committed.ContainsKey(character) || committed[character].Count == 0) {
+            return WantedStatus.NOT_WANTED;
+        } else {
+            return WantedStatus.DEAD_OR_ALIVE;
+        }
+    }
+
+    [System.Serializable] 
     private class SingleCrime {
         public string crime;
         public int bounty;
