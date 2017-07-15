8a005a906a350917070e6a4dd3a07786fd1786a1
Tyler Bonnell <tbonnell@uw.edu>
2017-07-15 14:34:13 -0700

    fix tîme cycle

--- Assets/Scripts/World/Atmosphere/TimeEffects.cs
+++ Assets/Scripts/World/Atmosphere/TimeEffects.cs
@@ -14,17 +14,21 @@ public class TimeEffects : MonoBehaviour {
 	}
 
 	private Color getColorForTime() {
-		float hour = (SaveGame.currentGame.time.worldTime % WorldTime.DAY / WorldTime.HOUR);
-		float timeSoFar = (SaveGame.currentGame.time.worldTime % WorldTime.HOUR);
+		float hour = (SaveGame.currentGame.time.worldTime % WorldTime.DAY) / WorldTime.HOUR;
+		float timeSoFar = SaveGame.currentGame.time.worldTime % WorldTime.HOUR;
 		float transitionTime = 45 * WorldTime.MINUTE;
-		float percentTransitioned = Mathf.Clamp01(timeSoFar/transitionTime);
-		if (hour >= 6 && hour < 20) {
-			return Color.Lerp(sunriseColor, dayColor, percentTransitioned);   // day	
-		} else if (hour >= 5 && hour < 6) {
+		// Debug.Log(timeSoFar + "/" + transitionTime + " = " + percentTransitioned);
+		if (hour >= 5 && hour < 6) {
+			float percentTransitioned = Mathf.Clamp01((timeSoFar + (hour - 5) * WorldTime.HOUR)/transitionTime);
 			return Color.Lerp(nightColor, sunriseColor, percentTransitioned); // sunrise		
+		} else if (hour >= 6 && hour < 20) {
+			float percentTransitioned = Mathf.Clamp01((timeSoFar + (hour - 6) * WorldTime.HOUR)/transitionTime);
+			return Color.Lerp(sunriseColor, dayColor, percentTransitioned);   // day	
 		} else if (hour >= 20 && hour < 21) {
+			float percentTransitioned = Mathf.Clamp01((timeSoFar + (hour - 20) * WorldTime.HOUR)/transitionTime);			
 			return Color.Lerp(dayColor, sunsetColor, percentTransitioned);    // sunset
 		} else {
+			float percentTransitioned = Mathf.Clamp01((timeSoFar + (24 + hour - 21) % 24 * WorldTime.HOUR)/transitionTime);			
 			return Color.Lerp(sunsetColor, nightColor, percentTransitioned);  // night			
 		}
 	}
