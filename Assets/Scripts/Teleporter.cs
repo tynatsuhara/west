6a8f5b3e2f764fd9aa9dc846802f9a85f68259ff
Tyler Bonnell <tbonnell@uw.edu>
2017-12-22 16:33:01 -0800

    remove teleporter debug

--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -49,7 +49,6 @@ public class Teleporter : MonoBehaviour {
 			// loading location autosaves before transport, so make sure to do any save modification (eg moving horses) after location load
 			GameManager.instance.LoadLocation(toId, 4 /* minutes per distance unit */ * dist * character.moveSpeed / character.CalculateSpeed());
 			ignore = true;
-			Debug.Log("Teleporting to " + otherSide.position.val);
 		} else {
 			// TODO: save character to the other location
 		}
