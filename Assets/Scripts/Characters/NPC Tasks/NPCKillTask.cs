8559a7ea5aaeb5a61c7ac978c6c0196c82f00165
Tyler Bonnell <tbonnell@uw.edu>
2018-01-09 10:50:36 -0800

    fix npc kill task for players vs npcs

--- Assets/Scripts/Characters/NPC Tasks/NPCKillTask.cs
+++ Assets/Scripts/Characters/NPC Tasks/NPCKillTask.cs
@@ -14,19 +14,20 @@ public class NPCKillTask : NPCTask {
     }
 
     public override Task.TaskDestination GetLocation() {
-        Character.CharacterSaveData c = GetCharacter();
-        return new Task.TaskDestination(Map.LocationOfCharacter(c.guid).guid, c.position.val);
+        if (target == System.Guid.Empty) {
+            return new Task.TaskDestination(Map.CurrentLocation().guid, SaveGame.currentGame.savedPlayers[0].position.val);
+        } else {
+            Character.CharacterSaveData c = SaveGame.currentGame.savedCharacters[target];
+            return new Task.TaskDestination(Map.LocationOfCharacter(c.guid).guid, c.position.val);
+        }
     }
     
     // Lower bound on time left for a task -- If this is <= 0, the task is done.
     public override float GetTimeLeft() {
-        return GetCharacter().health > 0 ? WorldTime.MINUTE : 0;
-    }
-
-    private Character.CharacterSaveData GetCharacter() {
         if (target == System.Guid.Empty) {
-            return SaveGame.currentGame.savedPlayers[0];
+            return SaveGame.currentGame.savedPlayers[0].health > 0 ? WorldTime.MINUTE : 0;
+        } else {
+            return SaveGame.currentGame.savedCharacters[target].health > 0 ? WorldTime.MINUTE : 0;
         }
-        return  SaveGame.currentGame.savedCharacters[target];
     }
 }
