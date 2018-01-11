fd29457db60aa70ba22e355d4fd5fb7942167e86
Tyler Bonnell <tbonnell@uw.edu>
2018-01-11 00:18:51 -0800

    add npcfollowtask

---
+++ Assets/Scripts/Characters/NPC Tasks/NPCFollowTask.cs
@@ -0,0 +1,32 @@
+using System;
+
+[System.Serializable]
+public class NPCFollowTask : NPCTask {
+
+    private System.Guid target;
+    public float distance;
+
+    // constructor to follow players
+    public NPCFollowTask(float distance) {
+        this.distance = distance;
+    }
+
+    public NPCFollowTask(float distance, System.Guid target) {
+        this.target = target;
+        this.distance = distance;
+    }
+
+    public override Task.TaskDestination GetLocation() {
+        if (target == System.Guid.Empty) {
+            return new Task.TaskDestination(Map.CurrentLocation().guid, SaveGame.currentGame.savedPlayers[0].position.val);
+        } else {
+            NPC.NPCSaveData c = SaveGame.currentGame.savedCharacters[target];
+            return new Task.TaskDestination(c.location, c.position.val);
+        }
+    }
+    
+    // Lower bound on time left for a task -- If this is <= 0, the task is done.
+    public override float GetTimeLeft() {
+        return WorldTime.MINUTE;
+    }
+}
