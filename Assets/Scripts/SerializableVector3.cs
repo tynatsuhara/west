89cd80974b5498aa85f55a284233303f42d5c785
Tyler Bonnell <tbonnell@uw.edu>
2018-02-14 10:54:13 -0800

    make sv3 more robust

--- Assets/Scripts/SerializableVector3.cs
+++ Assets/Scripts/SerializableVector3.cs
@@ -5,20 +5,30 @@ public class SerializableVector3 {
 	public float x, y, z;
 	public Vector3 val {
 		get { return new Vector3(x, y, z); }
+		set { 
+			x = value.x;
+			y = value.y;
+			z = value.z;
+		}
 	}
 
-	public SerializableVector3(Vector3 v) {
-		x = v.x;
-		y = v.y;
-		z = v.z;
+	public SerializableVector3(Vector3 v) : this(v.x, v.y, v.z) {
+	}
+
+	public SerializableVector3(float x, float y, float z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
 	}
 
 	public override bool Equals(System.Object obj) {
 		if (obj == null)
 			return false;
-		if (!(obj is SerializableVector3))
-			return false;
-		return val == ((SerializableVector3)obj).val;
+		if (obj is Vector3)
+			return val == (Vector3)obj;
+		if (obj is SerializableVector3)
+			return val == ((SerializableVector3)obj).val;
+		return false;
 	}
 
 	public override int GetHashCode() {
