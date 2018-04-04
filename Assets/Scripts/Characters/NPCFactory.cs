7aa69b64c580b6b31860be3a99878124fc263d32
Tyler Bonnell <tbonnell@uw.edu>
2018-04-04 15:29:21 -0700

    8-6 schedule

--- Assets/Scripts/Characters/NPCFactory.cs
+++ Assets/Scripts/Characters/NPCFactory.cs
@@ -7,8 +7,8 @@ public class NPCFactory {
 
         Schedule schedule = new Schedule()
             .AddBlock(WorldTime.HOUR * 8, new NPCNoOpTask(home.guid, home.RandomUnoccupiedTile()))
-            .AddBlock(WorldTime.HOUR * 8, new NPCNoOpTask(work.guid, work.RandomUnoccupiedTile()))
-            .AddBlock(WorldTime.HOUR * 8, new NPCNoOpTask(home.guid, home.RandomUnoccupiedTile()));
+            .AddBlock(WorldTime.HOUR * 10, new NPCNoOpTask(work.guid, work.RandomUnoccupiedTile()))
+            .AddBlock(WorldTime.HOUR * 6, new NPCNoOpTask(home.guid, home.RandomUnoccupiedTile()));
         npc.taskSources.Add(schedule);
 
         return npc;
