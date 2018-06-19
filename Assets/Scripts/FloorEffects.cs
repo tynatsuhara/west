96eefa703c284913a62fb3809df59c2a4a9f744b
Tyler Bonnell <tbonnell@uw.edu>
2018-06-19 07:58:39 -0700

    jumping particle effect

---
+++ Assets/Scripts/FloorEffects.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorEffects : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision) {
+		Jumper j = collision.transform.root.GetComponent<Jumper>();
+		if (j == null)
+			return;
+		Floor f = LevelBuilder.instance.FloorAt(j.transform.position);
+		if (f == null)
+			return;
+		f.KickUpDirtLanding(j.transform.position, j.landingDirtRadius);
+	}
+}
